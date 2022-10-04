98c1dc32874ad64b7c1f27b7ff1b4752cfe6336f
ExperiencedDevOps <service@contrasting.org>
2022-10-04 13:37:44 -0500

    Update main.py

--- stable/locust/locustfiles/example/main.py
+++ stable/locust/locustfiles/example/main.py
@@ -3,9 +3,12 @@
 from locust import HttpUser, task, between
 from lib.example_functions import choose_random_page
 from locust import events
+import names 
 
 
-default_headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
+default_headers = {'User-Agent': 'locust-test'} 
+                   
+                   # 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
         
 
 @events.test_start.add_listener
@@ -25,3 +28,14 @@ class WebsiteUser(HttpUser):
         self.client.get("/showtimes/list", headers=default_headers)
         self.client.get("/movies/list", headers=default_headers)
         self.client.get("/bookings/list", headers=default_headers)
+
+    @task(2) 
+    def add_user(self):
+
+       new_user = {'Name': 'foo', 'LastName': 'bar'}
+
+       new_user['Name'] = names.get_first_name()
+       new_user['LastName'] = names.get_last_name()
+
+       # headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+       self.client.post("/api/users/", json=new_user, headers=default_headers)
