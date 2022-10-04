2d7cae7660176546107ef443e26ac34685691ce4
ExperiencedDevOps <service@contrasting.org>
2022-10-04 09:58:11 -0500

    Update main.py

--- stable/locust/locustfiles/example/main.py
+++ stable/locust/locustfiles/example/main.py
@@ -2,10 +2,20 @@
 
 from locust import HttpUser, task, between
 from lib.example_functions import choose_random_page
+from locust import events
 
 
 default_headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
         
+
+@events.test_start.add_listener
+def on_test_start(environment, **kwargs):
+    print("A new test is starting")
+
+@events.test_stop.add_listener
+def on_test_stop(environment, **kwargs):
+    print("A new test is ending")
+    
 class WebsiteUser(HttpUser):
     wait_time = between(1, 2)
 
